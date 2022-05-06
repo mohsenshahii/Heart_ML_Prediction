926b71b65b3a55ac059f532b2c933b6e444c8281
MohsenShahi <mohsen1370shahi@gmail.com>
2022-05-06 16:30:44 +0200

    just testing the connection between git and VS

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,9 +1,21 @@
 #introducing needed libraries
-import pandas as pd
-import numpy as np
 import streamlit as st
 import seaborn as sb
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from time import time
+from sklearn.manifold import LocallyLinearEmbedding
+from sklearn.manifold import Isomap
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, precision_score
+from sklearn.tree import DecisionTreeClassifier
 
 #introducing Dataset
 heart_df = pd.read_csv('heart_2020_cleaned.csv')
