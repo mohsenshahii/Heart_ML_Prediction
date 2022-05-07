667f5cd3c85cc4e8a0430ff633230f340d04dc33
MohsenShahi <mohsen1370shahi@gmail.com>
2022-05-07 18:29:33 +0200

    calling a new func.

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -17,23 +17,19 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, precision_score
 from sklearn.tree import DecisionTreeClassifier
 
+from digitizer import digitizer
+
 #introducing Dataset
 heart_df = pd.read_csv('heart_2020_cleaned.csv')
 st.write('Heart Disease prediction')
 st.write(heart_df)
 
+heart_df_dg = digitizer(heart_df)
 
-heart_df['Diabetic'].replace('No, borderline diabetes', 1, inplace = True)
-heart_df['Diabetic'].replace('Yes', 2, inplace = True)
-heart_df['Diabetic'].replace('Yes (during pregnancy)', 3, inplace = True)
-heart_df.replace('Yes', 1, inplace = True)
-heart_df.replace('No', 0, inplace = True)
-heart_df['Sex'].replace(['Female', 'Male'], [0, 1], inplace = True)
-heart_df['Race'].replace(['White', 'Hispanic', 'Black', 'Other', 'Asian', 'American Indian/Alaskan Native'], [1, 2, 3, 4, 5, 6] , inplace = True)
-heart_df['GenHealth'].replace(['Very good','Good', 'Excellent', 'Fair', 'Poor'], [5, 4, 3, 2, 1], inplace = True)
-heart_df['AgeCategory'].replace(['18-24','25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80 or older'], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], inplace = True)
- 
-heart_corr = heart_df.corr()
+heart_corr = heart_df_dg.corr()
 fig,ax=plt.subplots(figsize=(10,6))
 sb.heatmap(heart_corr,annot=True,ax=ax)
 st.write(fig)
+
+ # cd source\repos\streamlit testing\streamlit testing\Heart
+ # streamlit run streamlit_testing.py
