dec138e7c2c49c52ef72f116ca4f8bff45f56758
MohsenShahi <mohsen1370shahi@gmail.com>
2022-05-13 19:05:09 +0200

    fixing some bugs in digitizer func.

--- digitizer.py
+++ digitizer.py
@@ -1,7 +1,10 @@
 import pandas as pd
+
 def digitizer(dataframe):
-    dataframe.replace(['Yes','No'],[1 , 0], inplace = True)
-    digitized = pd.get_dummies(dataframe)
-    y = dataframe['HeartDisease']
-    x = dataframe.drop('HeartDisease', axis=1)
-    return digitized
+  '''
+     The easier way to transforming categorical data to number was get_dummies function
+     This method of digitizing  slightly(1-2 percents) increase the accuracy of ML models
+  '''
+  dataframe.replace(['Yes','No'],[1 , 0], inplace = True)
+  digitized = pd.get_dummies(dataframe)
+  return digitized
