28e26deb7ff28312b59b4bb9ba10407bf41a7b16
MohsenShahi <mohsen1370shahi@gmail.com>
2022-05-13 19:07:27 +0200

    adding standadization func. and adding Feature extraction functions

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,4 +1,6 @@
-#introducing needed libraries
+###
+## import libraries
+###
 import streamlit as st
 import seaborn as sb
 import matplotlib.pyplot as plt
@@ -18,18 +20,65 @@ from sklearn.metrics import accuracy_score, precision_score
 from sklearn.tree import DecisionTreeClassifier
 
 from digitizer import digitizer
+from apply_model import apply_model
 
-#introducing Dataset
+
+###
+## Putting Dataset into a variable
+###
 heart_df = pd.read_csv('heart_2020_cleaned.csv')
 st.write('Heart Disease prediction')
-st.write(heart_df)
 
-heart_df_dg = digitizer(heart_df)
+###
+## Turning the categorical data into numerical with digitizer func. to prepare them for ML models
+###
+heart_df_digitized = digitizer(heart_df)
 
-heart_corr = heart_df_dg.corr()
+###
+## Splitting dataset into independent and dependent variables 
+###
+y = heart_df_digitized['HeartDisease']
+x = heart_df_digitized.drop('HeartDisease', axis=1)
+
+###
+## Standardizing Data
+###
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
+
+###
+## Feature Extraction with Principal Component Analysis (PCA)
+###
+# pca = PCA(n_components=2)
+# x_pca = pca.fit_transform(x_scaled)
+
+###
+## Feature Extraction with Linear Discriminant Analysis (LDA). LDA is a supervised learning classifier which means it requires both the features and the labels (or X and y). 
+###
+lda = LinearDiscriminantAnalysis(n_components=1)
+lda.fit(x_scaled, y)
+x_lda = lda.transform(x_scaled)
+
+###
+## Feature Extraction with Isomap. ISOmap is very time consuming so we prefer to not use it!
+###
+# iso = Isomap(n_components=2)
+# x_iso = iso.fit_transform(x_scaled)
+
+
+###
+## Applying the ML model on the data: DecisionTreeClassifier() GaussianNB()
+###
+model = AdaBoostClassifier(n_estimators=50,learning_rate=1)
+st.write(apply_model( x_lda, y, model))
+
+
+heart_corr = heart_df_digitized.corr()
 fig,ax=plt.subplots(figsize=(10,6))
 sb.heatmap(heart_corr,annot=True,ax=ax)
 st.write(fig)
 
- # cd source\repos\streamlit testing\streamlit testing\Heart
- # streamlit run streamlit_testing.py
+### 
+## cd source\repos\streamlit testing\streamlit testing\Heart
+## streamlit run streamlit_testing.py
+###
